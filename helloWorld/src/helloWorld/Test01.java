996ca412ef92542862370b2ee6f110c0e4a3d277
touriachi <touriachi@gmail.com>
2025-05-08 18:27:17 -0400

    test 01

---
+++ helloWorld/src/helloWorld/Test01.java
@@ -0,0 +1,12 @@
+package helloWorld;
+
+public class Test01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int i=1;
+        System.out.println(i);
+        
+        		
+    }
+}
